bf48fe4838e525ec9c67b24cc3ef53e5fde82c8e
PaGiJun <birdbox@kakao.com>
2024-08-29 11:19:58 +0900

    Improve web responsiveness

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
 from flask import Flask, render_template, request, jsonify
-from data_processing import fetch_all_jobs_data, fetch_all_major_data, preprocess_data
+import json
 from ramp_model import generate_career_recommendations
 
 app = Flask(__name__)
+
+# 미리 처리된 데이터를 로드
+with open('processed_jobs_data.json', 'r', encoding='utf-8') as f:
+    processed_jobs_data = json.load(f)
+
+with open('processed_major_data.json', 'r', encoding='utf-8') as f:
+    processed_major_data = json.load(f)
 
 @app.route('/')
 def index():
@@ -16,16 +23,6 @@ def recommend():
 
         if not user_input:
             return render_template('index.html', error="Please enter your interests.")
-
-        # API 키 정의
-        oapi_key = 'dce42638afaf57784a701d4b5371cdef'
-
-        # 직업 및 학과 정보 가져오기 및 전처리
-        jobs_data = fetch_all_jobs_data(oapi_key)
-        processed_jobs_data = preprocess_data(jobs_data, data_type='job')
-
-        major_data = fetch_all_major_data(oapi_key)
-        processed_major_data = preprocess_data(major_data, data_type='major')
 
         # 진로 추천 생성
         recommendations = generate_career_recommendations(processed_jobs_data, processed_major_data, user_input)
